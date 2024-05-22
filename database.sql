0cdc68256d2e2d1f52eac86e815e9b41c868d79a
Danilo Marques <danilomarques20@hotmail.com>
2024-05-22 00:09:53 -0300

    adding alter table to the database.sql file.

--- database.sql
+++ database.sql
@@ -2,7 +2,8 @@ create table if not exists fidus_master(
 	ID varchar(36) primary key,
 	name varchar(100) not null,
 	email varchar(100) unique not null,
-	password_hash varchar(100)
+	password_hash varchar(100),
+	created_at TIMESTAMP NOT NULL
 );
 
 create table if not exists fidus_password(
@@ -10,7 +11,12 @@ create table if not exists fidus_password(
 	master_id varchar(50) not null,
 	password bytea,
 	primary key(key, master_id),
+	created_at TIMESTAMP NOT NULL,
 	constraint fk_master foreign key(master_id) references fidus_master(ID)
 );
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+-- this should be executed inside the database server
+-- ALTER TABLE fidus_master ADD created_at TIMESTAMP NOT NULL DEFAULT NOW(); 
+-- ALTER TABLE fidus_password ADD created_at TIMESTAMP NOT NULL DEFAULT NOW(); 
